37bc0902a2c1b8cf20e28fc591d3c8412dc15411
Osniel Torres <osniel-torres@hotmail.com>
2025-03-08 23:28:53 -0500

    updated next config base paths

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ const nextConfig: NextConfig = {
   output: 'export', // Enables static export mode
   trailingSlash: true, // Ensures correct routing on GitHub Pages
   images: {
-    unoptimized: true, // Required for Next.js images in static mode
+    unoptimized: true, // Required for GitHub Pages
   },
+  basePath: '',
+  assetPrefix: '/', //
 };
 
 export default nextConfig;
