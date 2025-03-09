01a317305344e0ab403511a0ef34a1ea1fcdaded
Osniel Torres <osniel-torres@hotmail.com>
2025-03-08 22:30:25 -0500

    Fixed GitHub Pages basePath issue

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // Enables static export mode for Next.js
+  output: 'export', // Enables static export mode
   trailingSlash: true, // Ensures correct routing on GitHub Pages
   images: {
-    unoptimized: true, // Required for static exports with Next.js images
+    unoptimized: true, // Required for Next.js images in static mode
   },
+  basePath: '/lumon-token-web-app',
+  assetPrefix: '/lumon-token-web-app/',
 };
 
 export default nextConfig;
